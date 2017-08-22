a62de9b68b244f1494c60e202f290da7966d3677
Afaf Taddist <afaf.taddist71@gmail.com>
2017-08-22 20:34:33 +0100

    Lire la premiere line du fichier csv

---
+++ csv_analysis.py
@@ -0,0 +1,16 @@
+#Librairie standard pour utiliser des fonctionnalités qui dépendent du système d'exploitation
+import os
+
+def launch_analysis(data_file):
+	#we get the right path 
+	directory=os.path.dirname(__file__) 
+	# with this path, we go inside the folder `data` and get the file.
+	path_to_file=os.path.join(directory,"data",data_file) 
+
+	with open(path_to_file,"r") as f : 
+		preview= f.readline()
+
+	print("Yeah! We managed to read the file. Here is a preview: {}".format(preview))
+
+if __name__=="__main__":
+	launch_analysis('current_mps.csv')
